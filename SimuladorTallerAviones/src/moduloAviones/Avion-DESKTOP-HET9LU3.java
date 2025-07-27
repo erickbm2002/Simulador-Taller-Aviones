52e6c827e8c3a92bb28dfa9ce28a1b9b0f4d41e0
ERICK BARRIENTOS MONTES <eriickbarriientos42@gmail.com>
2025-07-26 22:33:41 -0600

    Se sube el proyecto

---
+++ SimuladorTallerAviones/src/moduloAviones/Avion-DESKTOP-HET9LU3.java
@@ -0,0 +1,6 @@
+public class Avion {
+    private String matricula;
+    private String modelo;
+    private String marca;
+    
+}
---
+++ SimuladorTallerAviones/src/moduloAviones/Avion.java
@@ -0,0 +1,16 @@
+public class Avion {
+    private String matricula;
+    private String modelo;
+    private String marca;
+    private int capacidadPasajeros;
+    private int anioFabricacion;
+
+    public Avion(String matricula, String modelo, String marca, int capacidadPasajeros, int anioFabricacion) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.capacidadPasajeros = capacidadPasajeros;
+        this.anioFabricacion = anioFabricacion;
+    }
+
+}
